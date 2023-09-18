70878229d69c64b5516a2243f256ed020c3887e8
Adrian <adrian.mokua@icloud.com>
2023-09-18 16:12:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define the companyName variable as a constant
+const companyName = 'Scuber';
+
+// Define the mostProfitableNeighborhood variable using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the companyCeo variable using let
+let companyCeo = 'Susan Smith';
